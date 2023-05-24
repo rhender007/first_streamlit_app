01277deadc7e6da66956b87a2819e3811a90ad92
Robert Henderson <35784314+rhender007@users.noreply.github.com>
2023-05-24 14:36:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,8 @@ streamlit.text(my_data_row)
 
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load contains:")
-streamlit.text(my_data_row)
+streamlit.header("The fruit load contains:")
+streamlit.dataframe(my_data_row)
 
 
 streamlit.title("My Parents New Healthy Diner")
