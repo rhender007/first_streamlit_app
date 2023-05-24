e0b54580e313fba74c3d468f1ce12522e736e553
Robert Henderson <35784314+rhender007@users.noreply.github.com>
2023-05-24 17:05:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,9 @@ from urllib.error import URLError
 
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('" + new_fruit + " ')")
         streamlit.text("insert into fruit_load_list values ('" + new_fruit + " ')")
+        my_cur.execute("insert into fruit_load_list values ('" + new_fruit + " ')")
+        
         return "Thanks for adding " + new_fruit
 
 
